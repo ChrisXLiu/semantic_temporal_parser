504c4ea4c0245c18f69fffe2798e01b1e02d138e
ChrisXLiu <zsuliuxi@gmail.com>
2023-03-12 16:21:35 -0700

    Added unit tests for the parsing logic. Fixed bugs caught by these tests. Left TODO about edge cases that need to be confirmed with the product requirements.

--- server/temporal_parsing.py
+++ server/temporal_parsing.py
@@ -2,6 +2,7 @@ from concurrent import futures
 from sutime import SUTime
 import grpc
 import datetime
+import re
 
 from temporal_parsing_pb2 import (
     TemporalParsingStatus,
@@ -21,15 +22,38 @@ class TemporalParsingService(
         return datetime.datetime.fromisoformat(iso_string)
 
     def format_time(time: datetime.time) -> datetime.time:
-        return time.strftime('%Y-%m-%d %H:%M')
+        return time.strftime("%Y-%m-%d %H:%M")
+
+    def is_implicit_next_week(utterance: str,
+                              today_date: datetime.datetime,
+                              parsed_time: datetime.time) -> bool:
+        DAY_OF_WEEK_REGEX = r'\b(mon|monday|tue|tuesday|wed|wednesday|thu|thur|thursday|fri|friday|sat|saturday|sun|sunday)\b'
+        return re.search(DAY_OF_WEEK_REGEX, utterance.lower()) is not None \
+            and parsed_time < today_date < parsed_time + datetime.timedelta(days=7)
 
     # Returns parsed time or None if unable to.
     def parse_internal(self, input: str, reference_date: str) -> datetime.time | None:
-        parsing_result = self.sutime.parse(input, reference_date)
-        if len(parsing_result) == 1 and parsing_result[0]["type"] == "TIME":
-            return TemporalParsingService.iso_string_to_time(parsing_result[0]["value"])
+        parsing_results = self.sutime.parse(input, reference_date)
+        if TemporalParsingService.is_fully_parsed(input, parsing_results):
+            return TemporalParsingService.iso_string_to_time(
+                parsing_results[0]["value"])
         else:
             return None
+
+    # Checks if the input string is parsed into a single recognized range,
+    # and there are no important words lie outside of the recognized range.
+    def is_fully_parsed(input: str, parsing_results):
+        if len(parsing_results) != 1:
+            return False
+        parsing_result = parsing_results[0]
+        type = parsing_result["type"]
+        start = parsing_result["start"]
+        end = parsing_result["end"]
+        # Non-stop words include relative terms, digits, months, days of weeks.
+        NON_STOP_WORDS_REGEX = r'[0-9]+|\b(last|next|upcoming|coming|week|month|jan|january|feb|feburary|mar|march|apr|april|may|jun|june|jul|july|aug|auguest|sep|sept|september|oct|october|nov|november|dec|december|mon|monday|tue|tuesday|wed|wednesday|thu|thur|thursday|fri|friday|sat|saturday|sun|sunday)\b'
+        return type == "TIME" \
+            and re.search(NON_STOP_WORDS_REGEX, input[0:start]) is None \
+            and re.search(NON_STOP_WORDS_REGEX, input[end:-1]) is None
 
     def Parse(self, request, context):
         try:
@@ -39,11 +63,26 @@ class TemporalParsingService(
                     status=TemporalParsingStatus.UNRECOGNIZED,
                     result="")
 
+            today_date = datetime.datetime.strptime(request.today, "%Y-%m-%d")
+            if parsed_time < today_date:
+                if TemporalParsingService.is_implicit_next_week(
+                        request.utterance, today_date, parsed_time):
+                    parsed_time += datetime.timedelta(days=7)
+                else:
+                    return TemporalParsingResponse(
+                        status=TemporalParsingStatus.UNRECOGNIZED,
+                        result="")
+
             return TemporalParsingResponse(
                 status=TemporalParsingStatus.SUCCEEDED,
                 result=TemporalParsingService.format_time(parsed_time))
+        except ValueError as e:
+            print(f'{e.__class__}:{e}')
+            return TemporalParsingResponse(
+                status=TemporalParsingStatus.UNRECOGNIZED,
+                result="")
         except Exception as e:
-            print(e)
+            print(f'{e.__class__}:{e}')
             return TemporalParsingResponse(
                 status=TemporalParsingStatus.SERVER_ERROR,
                 result="")
---
+++ server/temporal_parsing_test.py
@@ -0,0 +1,353 @@
+from temporal_parsing import TemporalParsingService
+
+from temporal_parsing_pb2 import TemporalParsingStatus, TemporalParsingRequest
+
+import unittest
+
+
+class TestParsingService(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._service = TemporalParsingService()
+
+    ########################################
+    # Group 1: Relative dates
+    def test_today_explicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="today at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-24 11:00")
+
+    def test_today_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-24 11:00")
+
+    def test_tomorrow(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="tomorrow at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:00")
+
+    def test_day_of_week_same_week(self):
+        # Note 2023-02-24 is a Friday
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Saturday at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:00")
+
+    def test_day_of_week_next_week_implicit(self):
+        # Note 2023-02-24 is a Friday
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Monday at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-27 11:00")
+
+    def test_day_of_week_next_week_explicit(self):
+        # Note 2023-02-24 is a Friday
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Next Tuesday at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-28 11:00")
+
+    def test_day_of_week_coming_week_explicit(self):
+        # Note 2023-02-24 is a Friday
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="The coming Wednesday at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-03-01 11:00")
+
+    # TODO: Do we need to handle "The upcoming"?
+    def test_day_of_week_upcoming_week_explicit(self):
+        # Note 2023-02-24 is a Friday
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="The upcoming Wednesday at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    # TODO: Do we need to handle "on the {}th of this month"?
+    def test_date_same_month(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On the 26th of this month at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    # TODO: Do we need to handle "on the {}th of next month"?
+    def test_date_next_month_explicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On the 3rd of the next month at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    # TODO: Do we need to handle this implicit case?
+    def test_date_next_month_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On the 3rd at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    # TODO: Do we need to handle "In x days"
+    def test_date_in_x_days(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="In 2 days at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    # TODO: Do we need to handle "In x days"
+    def test_date_in_x_days_cross_month_boundary(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="In 7 days at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    ########################################
+    # Group 2: Semi-absolute dates (assuming within a year)
+
+    def test_specific_date(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On March 3rd at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-03-03 11:00")
+
+    def test_specific_date_abbr_month(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Mar 3rd at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-03-03 11:00")
+
+    def test_specific_date_abbr_month_abbr_day(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Mar 3 at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-03-03 11:00")
+
+    def test_specific_date_cross_year_boundary_explicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Jan 3rd next year at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2024-01-03 11:00")
+
+    # TODO: Do we need to handle implict next year
+    def test_specific_date_cross_year_boundary_implict(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Jan 3rd at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_feb_29_leap_year(self):
+        request = TemporalParsingRequest(
+            today="2000-01-01", utterance="On Feb 29th at 11 am"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2000-02-29 11:00")
+
+    ########################################
+    # Group 3: Invalid dates
+
+    def test_invalid_feb_29_non_leap_year(self):
+        request = TemporalParsingRequest(
+            today="2023-01-01", utterance="On Feb 29th at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_invalid_feb_30(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Feb 30th at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_invalid_31(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On Apr 31st at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_invalid_32(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="On March 32nd at 11 am"
+        )
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    ########################################
+    # Group 4: Time expressions
+
+    def test_hour_only_am(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11 am"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:00")
+
+    def test_hour_minute_am(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11:25 am"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:25")
+
+    def test_hour_only_am_no_space(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11am"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:00")
+
+    def test_hour_minute_am_no_space(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11:25am"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:25")
+
+    def test_hour_only_pm(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 2 pm"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 14:00")
+
+    def test_hour_minute_pm(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 2:25 pm"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 14:25")
+
+    def test_hour_only_pm_no_space(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 2pm"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 14:00")
+
+    def test_hour_minute_pm_no_space(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 2:25pm"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 14:25")
+
+    # TODO: Do we need to handle such implicit cases?
+    def test_hour_only_am_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_hour_minute_am_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 11:30"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 11:30")
+
+    # TODO: Do we need to handle such implicit cases?
+    def test_hour_only_pm_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 15"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_hour_minute_pm_implicit(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 15:30"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 15:30")
+
+    # TODO: Do we need to handle this malformed format?
+    def test_single_digit_minute(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 15:5"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_hour_padded_with_zero(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 02:30pm"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.SUCCEEDED)
+        self.assertEqual(response.result, "2023-02-25 14:30")
+
+    ########################################
+    # Group 5: Invalid times
+
+    def test_out_of_bound_hour(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 25:00"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
+
+    def test_out_of_bound_minute(self):
+        request = TemporalParsingRequest(
+            today="2023-02-24", utterance="Tomorrow at 12:70"
+        )
+
+        response = __class__._service.Parse(request, None)
+        self.assertEqual(response.status, TemporalParsingStatus.UNRECOGNIZED)
